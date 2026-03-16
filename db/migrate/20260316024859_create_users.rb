929ca9b037428fe96401fa3576b4b04aa9b7b4ab
tomoki <kreva.diggy@gmail.com>
2026-03-16 11:55:30 +0900

    Userモデルのマイグレーション実行（nicknameの一意性制約を追加）

---
+++ db/migrate/20260316024859_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :nickname, null:false
+      t.string :password_digest, null:false
+      t.integer :notification_wday
+      t.time :notification_time
+
+      t.timestamps
+    end
+    add_index :users, :nickname, unique: true
+  end
+end
