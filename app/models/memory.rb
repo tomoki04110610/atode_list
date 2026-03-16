08517ab628742836b45a561cc8f02bec5caf2347
tomoki <kreva.diggy@gmail.com>
2026-03-16 12:27:55 +0900

    Memoryモデルに140文字制限と30/100件の上限チェックロジックを追加

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,3 +1,17 @@
 class Memory < ApplicationRecord
   belongs_to :user
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :status, presence: true
+  validates :check_memory_limit, on: :create
+
+  private
+  def check_memory_limit
+    # 未解決の(status :0)が30件以上の場合
+    if states == 0 && user.memories.where(status: 0).count >= 30
+       errors.add(:base, "未解決のメモが30件に達しています。調べて解決してから追加しましょう！")
+    # 履歴(status: 1)が100件以上ある場合
+    elsif states == 1 && user.memories.where(status: 1).count >= 100
+       errors.add(:base, "履歴が100件に達しています。不要なものを整理してください。")
+    end
+  end
 end
--- db/migrate/20260316031136_create_memories.rb
+++ db/migrate/20260316031136_create_memories.rb
@@ -1,8 +1,8 @@
 class CreateMemories < ActiveRecord::Migration[7.2]
   def change
     create_table :memories do |t|
-      t.string :content
-      t.integer :status
+      t.string :content, null: false
+      t.integer :status, null: false, default: 0
       t.references :user, null: false, foreign_key: true
       t.datetime :completed_at
 
