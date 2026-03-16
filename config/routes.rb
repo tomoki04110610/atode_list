dd51c09ac75bdfbc5fb5bce238f86572363995ba
tomoki <kreva.diggy@gmail.com>
2026-03-16 14:07:58 +0900

    ルーティングの設定完了（ログイン・ユーザー登録・ＰＷＡ標準設定）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root "sessions#new"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  resources :users, only: [:new, :create]
+
+  # PWA等の標準設定
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
