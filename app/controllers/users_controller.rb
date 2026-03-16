9eb6d8afafc0b52550bc004f57bf49573a196d35
tomoki <kreva.diggy@gmail.com>
2026-03-16 14:29:12 +0900

    Usersコントローラー実装とsignupルーティングの設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "ユーザー登録が完了しました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :password, :password_confirmation)
+  end
+end
