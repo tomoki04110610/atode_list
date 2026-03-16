1753f32231db93fd2dd582175c64301c59df65f0
tomoki <kreva.diggy@gmail.com>
2026-03-16 12:04:28 +0900

    Userモデルにバリデーションと暗号化を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :nickname, presence: true, uniqueness: true, length: { in: 3..15}
+  validates :password, presence: true, length: { in: 4..72 }, on: :create
 end
