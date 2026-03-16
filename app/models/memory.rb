07108eef8c5e07d8e3aba8be2b5988a245cce4f6
tomoki <kreva.diggy@gmail.com>
2026-03-16 13:02:42 +0900

    UserとMemoryのモデル設計およびバリデーション実装完了

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,15 +2,18 @@ class Memory < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
   validates :status, presence: true
-  validates :check_memory_limit, on: :create
+  validate :check_memory_limit, on: :create
 
   private
   def check_memory_limit
+    # userが紐づいていない場合（バリデーションエラー防止）
+    return unless user
+
     # 未解決の(status :0)が30件以上の場合
-    if states == 0 && user.memories.where(status: 0).count >= 30
+    if status == 0 && user.memories.where(status: 0).count >= 30
        errors.add(:base, "未解決のメモが30件に達しています。調べて解決してから追加しましょう！")
     # 履歴(status: 1)が100件以上ある場合
-    elsif states == 1 && user.memories.where(status: 1).count >= 100
+    elsif status == 1 && user.memories.where(status: 1).count >= 100
        errors.add(:base, "履歴が100件に達しています。不要なものを整理してください。")
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :memories, dependent: :destroy
+
   validates :nickname, presence: true, uniqueness: true, length: { in: 3..15}
   validates :password, presence: true, length: { in: 4..72 }, on: :create
 end
