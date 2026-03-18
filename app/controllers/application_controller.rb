d04aec9cace882dde0612e7128f964c16f61bd58
tomoki <kreva.diggy@gmail.com>
2026-03-18 10:46:51 +0900

    ApplicationControllerにcurrent_userとログイン制限実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in_user
+    unless logged_in?
+      redirect_to login_url, alert: "ログインしてください"
+    end
+  end
 end
